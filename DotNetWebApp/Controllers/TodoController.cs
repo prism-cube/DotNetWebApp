6b6c38da97bbe02901d06fa21ff51d73ff37b4e3
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-25 19:29:07 +0900

    初回コミット

---
+++ DotNetWebApp/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DotNetWebApp.Models;
+
+namespace DotNetWebApp.Controllers;
+
+public class TodoController : Controller
+{
+    private readonly ILogger<TodoController> _logger;
+
+    public TodoController(ILogger<TodoController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
+
