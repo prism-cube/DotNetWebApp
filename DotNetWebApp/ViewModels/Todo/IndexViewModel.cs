59cc58e95cd7f3fc4fead47525ba869eb3b192ac
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-28 02:32:41 +0900

    ViewModel Display Name

--- DotNetWebApp/ViewModels/Todo/IndexViewModel.cs
+++ DotNetWebApp/ViewModels/Todo/IndexViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetWebApp.ViewModels.Todo;
 
 public class IndexViewModel
 {
+    [Display(Name = "なまえ")]
     public string? Name { get; set; }
 
+    [Display(Name = "めた")]
     public int? Meta { get; set; }
 
+    [Display(Name = "すうじ")]
     public int? Number { get; set; }
 }
 
