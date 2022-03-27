262a9b31a68535faabba5cdc51e7aff5d7e4b4a1
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-28 01:03:30 +0900

    Detail Page

---
+++ DotNetWebApp/Requests/Todo/DetailRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using DotNetWebApp.Utils.Filters;
+
+namespace DotNetWebApp.Requests.Todo;
+
+public class IndexRequest : IValidatableObject
+{
+    [Display(Name = "なまえ")]
+    [Required(ErrorMessage = "{0}は必須です")]
+    [StringLength(10)]
+    public string? Name { get; set; }
+
+    [DisplayName("めた")]
+    [CustomRequired]
+    [MetaValidation]
+    public int? Meta { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Name == "NG")
+        {
+            yield return new ValidationResult("NameにNGは指定できません", new[] { nameof(Name) });
+        }
+
+        if (Name == "ng")
+        {
+            yield return new ValidationResult("Nameにngは指定できません", new[] { nameof(Name) });
+        }
+    }
+}
+
---
+++ DotNetWebApp/ViewModels/Todo/DetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DotNetWebApp.ViewModels.Todo;
+
+public class IndexViewModel
+{
+    public string? Name { get; set; }
+
+    public string? Meta { get; set; }
+}
+
