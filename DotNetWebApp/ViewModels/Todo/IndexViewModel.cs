577225ee07cd661350aeeb8f779f1ccffb7eb02a
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-28 03:12:19 +0900

    ViewModelのDisplay Name変更

--- DotNetWebApp/ViewModels/Todo/IndexViewModel.cs
+++ DotNetWebApp/ViewModels/Todo/IndexViewModel.cs
@@ -5,16 +5,16 @@ namespace DotNetWebApp.ViewModels.Todo;
 
 public class IndexViewModel
 {
-    [Display(Name = "なまえ")]
+    [Display(Name = "名前")]
     public string? Name { get; set; }
 
-    [Display(Name = "めた")]
+    [Display(Name = "メタ")]
     public int? Meta { get; set; }
 
-    [Display(Name = "すうじ")]
+    [Display(Name = "数字")]
     public int? Number { get; set; }
 
-    [Display(Name = "げんざいにちじ")]
+    [Display(Name = "現在日時")]
     [DataType(DataType.DateTime)]
     [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}", ApplyFormatInEditMode = true)]
     public DateTime? DateTimeNow { get; set; }
