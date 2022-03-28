6cc2bc3ae8e3a15937e299f5e5e77e292fc7d6b8
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-29 00:44:29 +0900

    入力チェックエラーのinputにBootStrap用のclassを付与

---
+++ DotNetWebApp/wwwroot/js/global.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let errorInputs = document.querySelectorAll('.input-validation-error');
+    errorInputs.forEach(function (e) {
+        // 入力チャックエラーのinputにBootStrap用のclassを付与
+        e.classList.add('is-invalid');
+    });
+});
