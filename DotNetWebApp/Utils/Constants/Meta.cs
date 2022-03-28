3d16b79624c6d0cf759b66cb27fa171b418faba4
prism-cube <ocean5014dev@yahoo.co.jp>
2022-03-28 15:25:53 +0900

    ViewComponentsサンプル

---
+++ DotNetWebApp/Utils/Constants/Meta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace DotNetWebApp.Utils.Constants;
+
+public static class Meta
+{
+    public static ReadOnlyDictionary<int, string> List { get; } = new ReadOnlyDictionary<int, string>(
+        new Dictionary<int, string> {
+                { 1, "hoge"},
+                { 2, "fuga"},
+                { 3, "piyo"},
+        }
+    );
+
+    public static bool ContainsKey(int key) => List.ContainsKey(key);
+}
+
---
+++ DotNetWebApp/ViewComponents/Sample/SampleViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWebApp.ViewComponents.Sample;
+
+public class SampleViewComponent: ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync(SampleViewComponentParam param)
+	{
+		var result = await GetTextAsync(param.Text);
+
+		var resultModel = new SampleViewComponentModel()
+		{
+			Text = result,
+        };
+		return View(resultModel);
+	}
+
+	private async Task<string> GetTextAsync(string text)
+    {
+		await Task.Run(() =>
+		{
+			Thread.Sleep(100);
+		});
+		return text;
+    }
+}
+
+public class SampleViewComponentParam
+{
+	public string Text { get; set; } = string.Empty;
+}
+
+public class SampleViewComponentModel
+{
+	public string? Text { get; set; }
+}
+
